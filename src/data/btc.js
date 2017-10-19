1f6e05b89d5b414231f431759eb523114e3340b4
Mark Lundin <mark@mark-lundin.com>
2017-10-19 14:19:00 +0100

    Added api to return data

---
+++ src/data/btc.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCkC_zpHJhgYkS-IbN_OwvZSjb4NfcN28g',
+  projectId: 'iohk-orpheus'
+  // authDomain: '### FIREBASE AUTH DOMAIN ###',
+})
+
+// Initialize Cloud Firestore through Firebase
+const blocks = firebase.firestore().collection('blocks')
+
+window.blocks = blocks
+
+/*
+  Get a list of BTC transaction over a time period
+*/
+export const getTransactionFeesOverTime = (start, end) => axios.get('https://api.blockchain.info/charts/transaction-fees?timespan=all&format=json&cors=true')
+  .then(({ data }) => data.values)
+
+export const getTransactionVolumeOverTime = (start, end) => axios.get('https://api.blockchain.info/charts/estimated-transaction-volume?format=json&cors=true')
+    .then(({ data }) => data.values)
+
+/*
+  Returns a block from a given hash
+*/
+export const getBlock = hash => blocks.where('hash', '==', hash)
+
+/*
+  Returns all the blocks that occured on the current date 00:01 - 00:00
+*/
+export const getBlocksOnDay = date => {
+  const fromDay = new Date(date)
+  fromDay.setMilliseconds(0)
+  fromDay.setSeconds(0)
+  fromDay.setMinutes(0)
+  fromDay.setHours(0)
+
+  const toDay = new Date(fromDay.getTime())
+  toDay.setHours(toDay.getHours() + 24)
+
+  return blocks.where('time', '>=', fromDay / 1000)
+    .where('time', '<', toDay / 1000)
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './Home'
+import { getBlocksOnDay, getTransactionFeesOverTime } from './data/btc'
 
 Vue.config.productionTip = false
 
@@ -11,6 +12,25 @@ const routes = [
 ]
 
 const router = new VueRouter({ routes, mode: 'history' })
+
+/*
+  Get all blocks that have occured today
+*/
+const today = Date.now()
+const blocks = []
+getBlocksOnDay(today).get().then(snapshot => {
+  snapshot.forEach(doc => blocks.push(doc.data()))
+})
+
+/*
+  Get a list of daily transaction fees
+*/
+getTransactionFeesOverTime().then(console.log)
+
+/*
+  Get an array of daily transaction values
+*/
+getTransactionVolumeOverTime().then(console.log)
 
 /* eslint-disable no-new */
 new Vue({ router }).$mount('#app')
