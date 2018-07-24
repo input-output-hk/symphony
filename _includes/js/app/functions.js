5257c4aebb3f18978fa3a4786a4b77650de0d6d2
Tomas Vrana <tomas.vrana@iohk.io>
2018-07-24 13:22:39 +0200

    gource click fix

--- _includes/js/app/functions.js
+++ _includes/js/app/functions.js
@@ -499,7 +499,7 @@ function io_which_way_alter(){
 	          git: {
 							owner: 'input-output-hk',
 							repo: 'symphony',
-							branch: 'develop',
+							branch: 'master',
 	            commitHash: '', // hash of commit to load
 	            commitDate: '', // date to load (YYYY-MM-DD)
 	            loadLatest: true // load latest commit in db
