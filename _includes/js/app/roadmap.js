813590a4d95a4dd0cd4f5fce72d7a4e4532fd86a
Tomas Vrana <tomas.vrana@iohk.io>
2018-09-11 13:57:33 +0200

    roadmap content on remote

--- _includes/js/app/roadmap.js
+++ _includes/js/app/roadmap.js
@@ -523,7 +523,7 @@ function rm_reload(){
     var url = '/js/content-jan.json';
     url = 'https://spreadsheets.google.com/feeds/list/1d_i5iMBZlFLhKRe4JJv47GDAmMDWrgWPF0nxF0k60to/od6/public/values?alt=json';
     //url = 'https://spreadsheets.google.com/feeds/list/14uD0DMi_HYpJkf4cSSGWDiGyMHoOPeoUArUW1uMmRuo/od6/public/values?alt=json';
-    url = '/static/assets/js/roadmap/values3.json';
+    //url = '/static/assets/js/roadmap/values3.json';
 
     $.getJSON(url, function(data) {
         var entry = data.feed.entry;
